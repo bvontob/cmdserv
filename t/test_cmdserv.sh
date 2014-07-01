16c3fc2937b8ceaf82c89ade60595e730d092350
Beat Vontobel <bvontob@users.noreply.github.com>
2014-07-01 20:23:51 +0000

    Improve test suite script, add notes about things that still need to improved, add test case markers to all output, add comments -- and an additional test case checking for too many connections.

--- t/test_cmdserv.sh
+++ t/test_cmdserv.sh
@@ -3,6 +3,14 @@
 CMDSERV_PORT=12346
 CMDSERV_HOST=::1
 
+TEST_OUT_CLIENT=t/test_cmdserv.conn
+TEST_OUT_STDERR=t/test_cmdserv.stderr
+TEST_OUT_STDOUT=t/test_cmdserv.stdout
+
+TEST_OUT_ALL="$TEST_OUT_CLIENT $TEST_OUT_STDERR $TEST_OUT_STDOUT"
+
+# TO DO: Instead of using netcat/nc, we should use our own tool based
+#        on clientlib.h/.c
 if which netcat >/dev/null 2>&1
 then
     NETCAT=netcat
@@ -14,13 +22,28 @@ else
     exit 1
 fi
 
-rm -f t/test_cmdserv.stdout t/test_cmdserv.stderr t/test_cmdserv.conn
+# TO DO: Check for server crashes between test cases.
+__TESTCASE__ () {
+    sleep 1 # Not nice. But to make sure previous output has been written.
+    echo "-- TESTCASE $1 --" \
+        | tee -ai \
+        $TEST_OUT_CLIENT \
+        $TEST_OUT_STDERR \
+        $TEST_OUT_STDOUT
+    SOURCE_PORT="$1""0001"
+}
 
+# Prepare empty test output files, so we can use append mode everywhere
+# and be sure that the markers and the real output get interleaved
+rm -f $TEST_OUT_ALL
+touch $TEST_OUT_ALL
+
+# Start the server to test with, possibly using valgrind
 `which valgrind >/dev/null && echo "valgrind -q --error-exitcode=99"` \
     ./t/test_cmdserv \
     <  /dev/null \
-    1> t/test_cmdserv.stdout \
-    2> t/test_cmdserv.stderr &
+    1>> t/test_cmdserv.stdout \
+    2>> t/test_cmdserv.stderr &
 
 SERVER_PID=$!
 
@@ -30,19 +53,39 @@ do
     sleep 1
 done
 
-echo "-- 1st --" >> t/test_cmdserv.conn
+
+################################################################################
+# Test cases
+################################################################################
+
+__TESTCASE__ 1
+
 printf "value get\r\nvalue set testcase\r\nvalue get\r\nexit\r\n" \
-    | $NETCAT -p10001 -q5 $CMDSERV_HOST $CMDSERV_PORT \
-    >> t/test_cmdserv.conn
-echo "-- 2nd --" >> t/test_cmdserv.conn
-printf "value get\r\nserver shutdown\r\n" \
-    | $NETCAT -p10002 -q5 $CMDSERV_HOST $CMDSERV_PORT \
+    | $NETCAT -p $SOURCE_PORT -q5 $CMDSERV_HOST $CMDSERV_PORT \
     >> t/test_cmdserv.conn
 
+
+__TESTCASE__ 2
+
+for SOURCE_PORT in 20011 20021
+do
+    t/too-many-connections $SOURCE_PORT $CMDSERV_HOST $CMDSERV_PORT
+done >> t/test_cmdserv.conn
+
+
+__TESTCASE__ 3
+
+printf "value get\r\nserver shutdown\r\n" \
+    | $NETCAT -p $SOURCE_PORT -q5 $CMDSERV_HOST $CMDSERV_PORT \
+    >> t/test_cmdserv.conn
+
+# TO DO: Abort after a short timeout, but continue with diffs
 wait $SERVER_PID || exit $?
 
+# TO DO: Remember exit code, but do not exit
 diff -u t/test_cmdserv.conn.exp   t/test_cmdserv.conn   || exit $?
 diff -u t/test_cmdserv.stderr.exp t/test_cmdserv.stderr || exit $?
 diff -u t/test_cmdserv.stdout.exp t/test_cmdserv.stdout || exit $?
 
+# TO DO: Delete files selectively (only those that are okay)
 rm -f t/test_cmdserv.stdout t/test_cmdserv.stderr t/test_cmdserv.conn
