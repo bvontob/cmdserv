eeea93fc6df674bbf0351832090f157012333efd
Beat Vontobel <bvontob@users.noreply.github.com>
2014-07-01 19:44:11 +0000

    Add support for a configurable source port to clientlib, so we can reproduce also the source port numbers across test runs.

--- t/clientlib.c
+++ t/clientlib.c
@@ -9,20 +9,34 @@
 #define DEFAULTPORT "12346"
 #define RELAYBUFLEN 4096
 
+int src_port = 0;
+
 int cmdserv_connect(int argc, char** argv) {
   struct addrinfo* ai;
+  char *host = NULL;
+  char *port = DEFAULTPORT;
   int ai_res;
   int fd = -1;
-  char host[HOSTBUFLEN], port[PORTBUFLEN];
+  char realhost[HOSTBUFLEN], realport[PORTBUFLEN];
 
-  if (argc > 3)
-    errx(EXIT_FAILURE, "Usage: %s [[HOST] PORT]", argc > 0 ? argv[0] : "BIN");
+  if (argc > 4) {
+    errx(EXIT_FAILURE,
+         "Usage: %s [[[SRC_PORT] HOST] PORT]",
+         argv[0]);
+  } else if (argc > 3) {
+    if (src_port == 0)
+      src_port = atoi(argv[1]);
+    host     = argv[2];
+    port     = argv[3];
+  } else if (argc > 2) {
+    host     = argv[1];
+    port     = argv[2];
+  } else if (argc > 1) {
+    port     = argv[1];
+  }
 
-  if ((ai_res = getaddrinfo((argc > 2 ? argv[1] :
-                             NULL),
-                            (argc > 2 ? argv[2] :
-                             argc > 1 ? argv[1] :
-                             DEFAULTPORT),
+  if ((ai_res = getaddrinfo(host,
+                            port,
                             &(struct addrinfo){
                               .ai_family   = AF_UNSPEC,
                               .ai_socktype = SOCK_STREAM,
@@ -37,19 +51,58 @@ int cmdserv_connect(int argc, char** argv) {
 
     if ((ai_res = getnameinfo(serv->ai_addr,
                               serv->ai_addrlen,
-                              host, sizeof(host),
-                              port, sizeof(port),
+                              realhost, HOSTBUFLEN,
+                              realport, PORTBUFLEN,
                               NI_NUMERICHOST | NI_NUMERICSERV)) != 0)
       errx(EXIT_FAILURE, "getaddrinfo(): %s", gai_strerror(ai_res));
-    info("Trying %s port %s...", host, port);
+
+    if (src_port > 0)
+      info("Trying %s destination port %s source port %d...",
+           realhost, realport, src_port);
+    else
+      info("Trying %s port %s...", realhost, realport);
     
     if ((fd = socket(serv->ai_family,
-                          serv->ai_socktype,
-                          serv->ai_protocol)) == -1) {
+                     serv->ai_socktype,
+                     serv->ai_protocol)) == -1) {
       warn("socket()");
       continue;
     }
     
+    if (src_port > 0) {
+      if (setsockopt(fd, SOL_SOCKET,
+                     SO_REUSEADDR, &(int){1}, sizeof(int))
+          == -1) {
+        warn("setsockopt(SO_REUSEADDR)");
+        close(fd);
+        fd = -1;
+        continue;
+      }
+
+
+      if ((serv->ai_family == AF_INET6
+           ? bind(fd,
+                  (struct sockaddr*)&(struct sockaddr_in6){
+                    .sin6_family      = AF_INET6,
+                    .sin6_addr        = IN6ADDR_ANY_INIT,
+                    .sin6_port        = htons(src_port)
+                  },
+                  sizeof(struct sockaddr_in6))
+           : bind(fd,
+                  (struct sockaddr*)&(struct sockaddr_in){
+                    .sin_family      = AF_INET,
+                    .sin_addr.s_addr = INADDR_ANY,
+                    .sin_port        = htons(src_port)
+                  },
+                  sizeof(struct sockaddr_in)))
+          != 0) {
+        warn("bind()");
+        close(fd);
+        fd = -1;
+        continue;
+      }
+    }
+
     if (connect(fd, serv->ai_addr, serv->ai_addrlen) != 0) {
       warn("connect()");
       close(fd);
@@ -59,6 +112,9 @@ int cmdserv_connect(int argc, char** argv) {
   }
 
   freeaddrinfo(ai);
+
+  if (src_port > 0)
+    src_port++;
 
   if (fd == -1)
     errx(EXIT_FAILURE, "Connection failed.");
