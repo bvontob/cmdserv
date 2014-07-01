1cc52156193d958021405e8a862329db6fbd38c6
Beat Vontobel <bvontob@users.noreply.github.com>
2014-07-01 19:39:04 +0000

    Try a version of cmdserv_logger_stderr() that uses write() internally instead of dprintf(), to maybe get more synchronous output in test cases. (This default logger is used mostly for test cases and debugging anyway.)

--- cmdserv_logger.c
+++ cmdserv_logger.c
@@ -1,6 +1,9 @@
 #include "cmdserv_logger.h"
 
+#define _GNU_SOURCE /* for asprintf() in stdio.h */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 char *cmdserv_logseverity_string(enum cmdserv_logseverity severity) {
   switch (severity) {
@@ -22,7 +25,21 @@ void cmdserv_logger_stderr(void* object,
                            const char* msg) {
   (void)object; /* UNUSED */
 
-  dprintf(2, "cmdserv <%s>: %s\n",
-          cmdserv_logseverity_string(severity),
-          msg);
+  ssize_t len;
+  char *buf;
+
+  /*
+   * Instead of using dprintf() or similar means, we build the string
+   * in memory and send it to STDERR using write().  This built-in
+   * logging to STDERR will mainly be used for debugging, and in our
+   * test suite.  And write() should guarantee us an atomic write
+   * (dprintf() on the other hand messed up some of the test output).
+   */
+  if ((len = asprintf(&buf, "cmdserv <%s>: %s\n",
+                      cmdserv_logseverity_string(severity),
+                      msg))
+      > 0) {
+    write(STDERR_FILENO, buf, len);
+    free(buf);
+  }
 }
