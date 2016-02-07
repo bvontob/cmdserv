ec724de406fcc5a07a99198f27fe0414cdd124c5
Beat Vontobel <bvontob@users.noreply.github.com>
2016-02-07 09:50:36 +0000

    Also intercept select() and getnameinfo()

--- intercept.h
+++ intercept.h
@@ -12,15 +12,17 @@ enum intercept_funcs {
 };
 
 #ifdef INTERCEPT
-#define calloc(...)     INTERCEPT_FUNC(calloc)(__VA_ARGS__)
-#define malloc(...)     INTERCEPT_FUNC(malloc)(__VA_ARGS__)
-#define realloc(...)    INTERCEPT_FUNC(realloc)(__VA_ARGS__)
-#define socket(...)     INTERCEPT_FUNC(socket)(__VA_ARGS__)
-#define recv(...)       INTERCEPT_FUNC(recv)(__VA_ARGS__)
-#define setsockopt(...) INTERCEPT_FUNC(setsockopt)(__VA_ARGS__)
-#define listen(...)     INTERCEPT_FUNC(listen)(__VA_ARGS__)
-#define bind(...)       INTERCEPT_FUNC(bind)(__VA_ARGS__)
-#define accept(...)     INTERCEPT_FUNC(accept)(__VA_ARGS__)
+#define calloc(...)        INTERCEPT_FUNC(calloc)(__VA_ARGS__)
+#define malloc(...)        INTERCEPT_FUNC(malloc)(__VA_ARGS__)
+#define realloc(...)       INTERCEPT_FUNC(realloc)(__VA_ARGS__)
+#define select(...)        INTERCEPT_FUNC(select)(__VA_ARGS__)
+#define socket(...)        INTERCEPT_FUNC(socket)(__VA_ARGS__)
+#define recv(...)          INTERCEPT_FUNC(recv)(__VA_ARGS__)
+#define setsockopt(...)    INTERCEPT_FUNC(setsockopt)(__VA_ARGS__)
+#define listen(...)        INTERCEPT_FUNC(listen)(__VA_ARGS__)
+#define bind(...)          INTERCEPT_FUNC(bind)(__VA_ARGS__)
+#define accept(...)        INTERCEPT_FUNC(accept)(__VA_ARGS__)
+#define getnameinfo(...)   INTERCEPT_FUNC(getnameinfo)(__VA_ARGS__)
 #endif /* INTERCEPT */
 
 #define EXPAND_INTERCEPTOR(ret_type, func_name, ...)     \
